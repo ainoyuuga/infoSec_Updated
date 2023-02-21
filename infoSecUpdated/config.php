59bb2a01d18a57d182340efc8268db5047a87004
ainoyuuga <107828620+ainoyuuga@users.noreply.github.com>
2023-02-21 21:57:37 +0800

    Update config.php

--- infoSecUpdated/config.php
+++ infoSecUpdated/config.php
@@ -3,7 +3,7 @@
 $databaseHost = 'localhost';
 $databaseName = 'blogsite';
 $databaseUsername = 'root';
-$databasePassword = 1234;
+$databasePassword = 1234; //change this section if you are not using password
 
 $mysqli = mysqli_connect($databaseHost,$databaseUsername,$databasePassword, $databaseName);
 ?>
